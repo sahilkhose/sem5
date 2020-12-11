c875479d590145a963b70179b102d8323430e23b
Sahil Khose <sahilkhose18@gmail.com>
2020-12-11 18:32:35 +0530

    Update db code

--- AP/lab6/3.py
+++ AP/lab6/3.py
@@ -1,11 +1,20 @@
 import sqlite3
 conn = sqlite3.connect('example.db')
+print("Database connected.")
 
 c = conn.cursor()
 
-c.execute('''CREATE TABLE students(name text, reg_no real, email text, ph_no real)''')
-c.execute("INSERT INTO students VALUES ('sahil',180953218,'sahil@gmail.com',100100100)")
+c.execute('''CREATE TABLE students
+	(ID INT PRIMARY KEY		NOT NULL, 
+	NAME TEXT 	NOT NULL, 
+	email CHAR(50)	NOT NULL, 
+	ph_no INT);''')
+print("Table created.")
+
+
+c.execute("INSERT INTO students VALUES (180953218, 'sahil', 'sahil@gmail.com',100100100)")
+c.execute("INSERT INTO students VALUES (180953222, 'abhinav', 'abhinav@gmail.com',100100101)")
 
 conn.commit()
-
+print("Records created.")
 conn.close()
