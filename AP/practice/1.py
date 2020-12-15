2a238cf6f0d0baed64d17d7c685a4924d740820b
Sahil Khose <sahilkhose18@gmail.com>
2020-12-15 08:01:28 +0530

    Update practice script

--- AP/practice/1.py
+++ AP/practice/1.py
@@ -3,9 +3,9 @@ from tkinter import *
 from inspect import *
 
 win = Tk()
-win.title("Sahil practice")
-win.geometry('300x300')
-win.configure(background = 'blue')
+# win.title("Sahil practice")
+# win.geometry('300x300')
+# win.configure(background = 'blue')
 
 # print(dir(Button))
 # print(getmembers(Button))
@@ -14,6 +14,6 @@ print(signature(Button))
 
 l1 = Label(win, text="Name").grid(row=0, column=0)
 e1 = Entry(win).grid(row=0, column=1)
-btn = Button(win, text="name").grid(row=1, column=1)
+btn = Button(win, text="name", command=sys.exit).grid(row=1, column=1)
 
 win.mainloop()
